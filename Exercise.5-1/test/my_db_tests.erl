330caf155c24b3a15655b2c18502a3aa1d3f42d9
jgarciasacristan <jose.garciasacristan@gmail.com>
2014-09-30 15:35:54 -0700

    WIP. Trying to write proper tests

---
+++ Exercise.5-1/test/my_db_tests.erl
@@ -0,0 +1,37 @@
+-module(my_db_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+start() ->
+    my_db:start(),
+    my_db:write(francesco,london),
+    my_db:write(lelle, stockholm),
+    my_db:write(joern, stockholm).
+
+stop(_) ->
+    my_db:stop().
+
+
+%read_test_() -> 
+%  {setup,
+%   fun start/0,
+%   fun stop/1,
+%    [?_assertEqual({ok,london}, my_db:read(francesco)),
+%        ?_assertEqual({ok,stockholm},my_db:read(joern)),
+%	 ?_assertEqual({error,instance}, my_db:read(jose))]}.
+
+
+%
+%match_test_() -> {
+%        setup,
+%        fun start/0,
+%        fun stop/1,
+%        [?_assertEqual([lelle, joern], my_db:match(stockholm)),
+%        ?_assertEqual([francesco],my_db:match(london)),
+%	 ?_assertEqual([], my_db:match(dublin))]}.
+
+delete_test_() -> {
+        setup,
+        fun start/0,
+        fun stop/1,
+        [?_assertEqual([{joern, stockholm},{francesco,london}], my_db:delete(lelle)),
+         ?_assertEqual([{joern, stockholm},{lelle,stockholm},{francesco,london}], my_db:delete(foo))]}.
