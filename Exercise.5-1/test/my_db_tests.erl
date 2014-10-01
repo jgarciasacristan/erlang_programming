efb9c53866538c812f593618de82ddb64046968e
jgarciasacristan <jose.garciasacristan@gmail.com>
2014-10-01 05:56:50 -0700

    WIP: fixing tests

--- Exercise.5-1/test/my_db_tests.erl
+++ Exercise.5-1/test/my_db_tests.erl
@@ -7,31 +7,30 @@ start() ->
     my_db:write(lelle, stockholm),
     my_db:write(joern, stockholm).
 
-stop(_) ->
-    my_db:stop().
+ read_test_() -> 
+  {setup,
+   fun start/0,
+   fun stop/1,
+   [?_assertEqual({ok,london}, my_db:read(francesco)),
+        ?_assertEqual({ok,stockholm},my_db:read(joern)),
+	 ?_assertEqual({error,instance}, my_db:read(jose))]}.
 
 
-%read_test_() -> 
-%  {setup,
-%   fun start/0,
-%   fun stop/1,
-%    [?_assertEqual({ok,london}, my_db:read(francesco)),
-%        ?_assertEqual({ok,stockholm},my_db:read(joern)),
-%	 ?_assertEqual({error,instance}, my_db:read(jose))]}.
+match_test_() -> {setup,
+        fun start/0,
+        fun stop/1,
+         [?_assertEqual([lelle, joern], my_db:match(stockholm)),
+        ?_assertEqual([francesco],my_db:match(london)),
+	 ?_assertEqual([], my_db:match(dublin))]}.
 
 
-%
-%match_test_() -> {
-%        setup,
-%        fun start/0,
-%        fun stop/1,
-%        [?_assertEqual([lelle, joern], my_db:match(stockholm)),
-%        ?_assertEqual([francesco],my_db:match(london)),
-%	 ?_assertEqual([], my_db:match(dublin))]}.
-
-delete_test_() -> {
-        setup,
+delete_test_() -> 
+       {setup,
         fun start/0,
         fun stop/1,
         [?_assertEqual([{joern, stockholm},{francesco,london}], my_db:delete(lelle)),
          ?_assertEqual([{joern, stockholm},{lelle,stockholm},{francesco,london}], my_db:delete(foo))]}.
+
+stop(_Pid) ->
+   my_db:stop(),
+   ?debugMsg("after stop").
